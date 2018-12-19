1d7b5e4ed823fec1ab375b4eb0afa634b44947e5
armand chariot <armand@chariot.be>
2018-12-19 15:52:06 +0100

    creating sing_in facebook button (gem omniauth-facebook-def in user model- omniauth_callbacks_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get "/log_in" => "devise/sessions#new" # custom path to login/sign_in
     get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
   end
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   root to: "pages#home"
 end
