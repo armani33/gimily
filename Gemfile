4f02de2a99d8171808b6adb59e08a922a3a90ae5
armand chariot <armand@chariot.be>
2018-12-19 17:27:58 +0100

    login with facebook button

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'autoprefixer-rails'
 gem 'jquery-rails'
 gem 'omniauth-facebook'
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use Puma as the app server
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,21 +11,22 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:facebook]
 
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
+  def self.find_for_facebook_oauth(auth)
+      user_params = auth.slice(:provider, :uid)
+      user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+      user_params[:facebook_picture_url] = auth.info.image
+      user_params = user_params.to_h
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.uid = auth.uid
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user = User.find_by(provider: auth.provider, uid: auth.uid)
+      user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
+      if user
+        user.update(user_params)
+      else
+        user = User.new(user_params)
+        user.password = Devise.friendly_token[0,20]  # Fake password for validation
+        user.save
+      end
+
+      return user
     end
-  end
 end
